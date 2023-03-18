0f6b1fb77a921c929d29f7e7bced74f2146d6ca4
Vadim Darchuk <vadim6385@gmail.com>
2023-03-18 19:31:39 +0200

    further additions in test code

--- test.py
+++ test.py
@@ -4,10 +4,11 @@ from user import UserType, User
 # Create some User objects
 user1 = User(UserType.REGULAR, 2, demand=1)
 user2 = User(UserType.PREMIUM, 10, demand=2)
-user3 = User(UserType.ENTERPRISE, 20, demand=3)
+user3 = User(UserType.ENTERPRISE, 50, min_bandwidth=10, demand=10)
+user4 = User(UserType.REGULAR, 5, demand=5)
 
 # Create an ISP object and add the users to it
-isp = ISP(30, [user1, user2, user3])
+isp = ISP(100, [user1, user2, user3, user4])
 
 # Allocate bandwidth to the users
 isp.allocate_bandwidth()

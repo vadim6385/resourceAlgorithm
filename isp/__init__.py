28b5a34893d80ec3fbfeadef61383f2caca3b17c
Vadim Darchuk <vadim.darchuk@wdc.com>
2023-04-25 14:41:52 +0300

    fix recursion error

--- isp/__init__.py
+++ isp/__init__.py
@@ -6,7 +6,7 @@ class ISP:
         self.users = users
         self.total_bandwidth = total_bandwidth
 
-    def allocate_bandwidth(self, time_window, users=None):
+    def allocate_bandwidth(self, time_window, users=None, remaining_bandwidth=-1):
         if users is None:
             users = self.users
 
@@ -19,7 +19,7 @@ class ISP:
         # Sort users by descending UserType and demand
         active_users.sort(key=lambda u: (int(u.user_type), u.demand), reverse=True)
 
-        remaining_bandwidth = self.total_bandwidth
+        remaining_bandwidth = self.total_bandwidth if remaining_bandwidth == -1 else remaining_bandwidth
         min_bandwidth_sum = sum(u.min_bandwidth for u in active_users)
 
         if min_bandwidth_sum >= remaining_bandwidth:
@@ -29,7 +29,7 @@ class ISP:
 
         for user in active_users:
             ideal_bandwidth = remaining_bandwidth * user.demand / sum(u.demand for u in active_users)
-            user.bandwidth_allocation = max(min(ideal_bandwidth, user.demand), user.min_bandwidth)
+            user.bandwidth_allocation += max(min(ideal_bandwidth, user.demand), user.min_bandwidth)
             remaining_bandwidth -= user.bandwidth_allocation
 
         # check if all users got their demand, if yes, quit:
@@ -41,7 +41,7 @@ class ISP:
 
         unsatisfied_users = [user for user in active_users if user.demand > user.bandwidth_allocation]
         if unsatisfied_users:
-            self.allocate_bandwidth(time_window, users=unsatisfied_users)
+            self.allocate_bandwidth(time_window, users=unsatisfied_users, remaining_bandwidth=remaining_bandwidth)
 
     def display_bandwidth_allocation(self):
         for user in self.users:
--- test.py
+++ test.py
@@ -7,14 +7,14 @@ def main():
 
     users = [
         User(UserType.REGULAR, demand=100, start_time=0, end_time=10),
-        User(UserType.PREMIUM, demand=250, start_time=5, end_time=15),
+        User(UserType.PREMIUM, demand=250, start_time=5, end_time=20),
         User(UserType.ENTERPRISE, demand=500, min_bandwidth=50, start_time=10, end_time=20),
-        User(UserType.REGULAR, demand=400, start_time=0, end_time=10),
-        User(UserType.PREMIUM, demand=300, start_time=5, end_time=15),
+        User(UserType.REGULAR, demand=400, start_time=0, end_time=5),
+        User(UserType.PREMIUM, demand=300, start_time=5, end_time=10),
         User(UserType.ENTERPRISE, demand=700, min_bandwidth=50, start_time=10, end_time=20),
         User(UserType.REGULAR, demand=50, start_time=0, end_time=10),
-        User(UserType.PREMIUM, demand=300, start_time=5, end_time=15),
-        User(UserType.ENTERPRISE, demand=900, min_bandwidth=50, start_time=10, end_time=20),
+        User(UserType.PREMIUM, demand=300, start_time=0, end_time=25),
+        User(UserType.ENTERPRISE, demand=900, min_bandwidth=50, start_time=0, end_time=25),
     ]
 
     isp = ISP(users, total_bandwidth)

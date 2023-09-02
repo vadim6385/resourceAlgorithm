3615474752ca1924e4da8bc40e6dbf08bf5f1952
Vadim Darchuk <vadim.darchuk@wdc.com>
2023-09-02 16:26:53 +0300

    add heatmap plot

--- algo_tester.py
+++ algo_tester.py
@@ -1,11 +1,13 @@
 import task
 import numpy as np
 
+from heatmap_plot import TaskHeatmap
 from utils import DEBUG_HALT
 
 
 class AlgoTester:
     def __init__(self, task_list_file, total_bandwith):
+        self.task_matrix = None
         self.total_bandwidth = total_bandwith
         self.task_list = task.from_json_file(task_list_file)
         self.completed_tasks = []
@@ -19,12 +21,13 @@ class AlgoTester:
         # get earliest task scheduled start time and latest task completion time
         self.time_start = min(one_task.created_time for one_task in self.completed_tasks)
         self.time_end = max(one_task.actual_end_time for one_task in self.completed_tasks)
-        self.rate_tasks() # calculate score for each task
+        self.rate_tasks()  # calculate score for each task
         self.create_task_matrix()
 
     def rate_tasks(self):
         priority_names = [name for name, member in task.TaskPriority.__members__.items()]
-        self.scores_dict = { priority : [0,0,0] for priority in priority_names} # { "PRIORITY" : [num_tasks, total_score, avg_score] }
+        self.scores_dict = {priority: [0, 0, 0] for priority in
+                            priority_names}  # { "PRIORITY" : [num_tasks, total_score, avg_score] }
         for one_task in self.completed_tasks:
             orig_start_time = one_task.created_time
             actual_start_time = one_task.actual_start_time
@@ -49,20 +52,26 @@ class AlgoTester:
         return ret
 
     def create_task_matrix(self):
-        self.task_matrix = np.zeros((self.total_bandwidth, self.time_end+1), dtype=int)
+        self.task_matrix = np.zeros((self.total_bandwidth, self.time_end + 1), dtype=int)
         for one_task in self.completed_tasks:
-            for time in range(one_task.actual_start_time, one_task.actual_end_time+1):
+            for time in range(one_task.actual_start_time, one_task.actual_end_time + 1):
                 allocated = False
                 for bw in range(self.total_bandwidth):
-                    if all(self.task_matrix[bw:bw+one_task.bandwidth, time] == 0):
-                        self.task_matrix[bw:bw+one_task.bandwidth, time] = one_task.id
+                    if all(self.task_matrix[bw:bw + one_task.bandwidth, time] == 0):
+                        self.task_matrix[bw:bw + one_task.bandwidth, time] = one_task.id
                         allocated = True
                         break
                 if not allocated:
                     DEBUG_HALT()
         # sort the matrix
-        for time in range(self.time_end+1):
+        for time in range(self.time_end + 1):
             self.task_matrix[:, time] = np.sort(self.task_matrix[:, time])
+
+    def show_heatmap_plot(self):
+        if self.task_matrix is None:
+            raise Exception("Plot can be shown only after tasks have been ran")
+        heatmap_plot = TaskHeatmap(task_matrix=self.task_matrix)
+        heatmap_plot.show_plot()
 
 
 if __name__ == "__main__":
@@ -71,8 +80,10 @@ if __name__ == "__main__":
     tester = AlgoTester(task_list, 50)
     tester.test(simple_greedy_algorithm)
     print(f"Greedy algorithm average score: {tester.avg_score_per_priority_str()}")
-    del(tester)
+    tester.show_heatmap_plot()
+    del (tester)
     from algorithms import greedy_compression_algorithm
     tester = AlgoTester(task_list, 50)
     tester.test(greedy_compression_algorithm)
     print(f"Greedy compression algorithm average score: {tester.avg_score_per_priority_str()}")
+    tester.show_heatmap_plot()
---
+++ heatmap_plot.py
@@ -0,0 +1,84 @@
+import numpy as np
+import seaborn as sns
+from matplotlib import pylab as plt
+
+
+class TaskHeatmap:
+    # Initialize the class with task_matrix, dropped_tasks_num, and dropped_tasks_ratio
+    def __init__(self, task_matrix):
+        self.task_matrix = task_matrix
+
+    # Function to find the center of rectangles in the heatmap
+    def find_rectangles_center(self):
+        rectangles = {}  # Dictionary to store the center of rectangles
+        visited = np.zeros_like(self.task_matrix, dtype=bool)  # Matrix to keep track of visited cells
+
+        # Loop through each cell in the task_matrix
+        for y in range(self.task_matrix.shape[0]):
+            for x in range(self.task_matrix.shape[1]):
+                # If the cell is not visited
+                if not visited[y, x]:
+                    value = self.task_matrix[y, x]
+                    y_min, y_max, x_min, x_max = y, y, x, x
+
+                    # Expand the rectangle horizontally
+                    for x2 in range(x + 1, self.task_matrix.shape[1]):
+                        if self.task_matrix[y, x2] == value and not visited[y, x2]:
+                            x_max = x2
+                            visited[y, x2] = True
+                        else:
+                            break
+
+                    # Expand the rectangle vertically
+                    for y2 in range(y + 1, self.task_matrix.shape[0]):
+                        if (self.task_matrix[y2, x:x_max + 1] == value).all() and not visited[y2, x:x_max + 1].any():
+                            y_max = y2
+                            visited[y2, x:x_max + 1] = True
+                        else:
+                            break
+
+                    # Store the center of the rectangle
+                    rectangles[value] = ((y_min + y_max) / 2, (x_min + x_max) / 2)
+                    visited[y:y_max + 1, x:x_max + 1] = True
+
+        return rectangles
+
+    # Function to display the heatmap
+    def show_plot(self):
+        fig, ax = plt.subplots()
+        heatmap = sns.heatmap(self.task_matrix, cmap='Greens', ax=ax)
+        plt.title("Task allocation graph",
+                  fontsize=20)
+        plt.xlabel("t(sec)")
+        plt.ylabel("Bandwidth(Mbps)")
+
+        centers = self.find_rectangles_center()  # Get the centers of rectangles
+
+        # Define the color and font size ranges
+        min_value, max_value = self.task_matrix.min(), self.task_matrix.max()
+        min_font_size, max_font_size = 10, 20
+        color_map = plt.cm.get_cmap('Greys')
+
+        # Annotate each rectangle with its value
+        for value, (y_center, x_center) in centers.items():
+            # Normalize the value to a range between 0 and 1
+            normalized_value = (value - min_value) / (max_value - min_value)
+
+            # Calculate font size based on normalized value
+            font_size = min_font_size + normalized_value * (max_font_size - min_font_size)
+
+            # Calculate text color based on the value
+            text_color = color_map(1 - normalized_value)
+
+            # Add text annotation to the heatmap
+            heatmap.text(x_center + 0.5, y_center + 0.5, value, horizontalalignment='center',
+                         verticalalignment='center', color=text_color, fontsize=font_size, fontweight='bold')
+
+        plt.show()  # Display the heatmap
+
+
+# Example usage
+if __name__ == "__main__":
+    task_matrix = np.array([[1, 2, 3], [4, 5, 6], [7, 8, 9]])
+    heatmap = TaskHeatmap(task_matrix)
+    heatmap.show_plot()

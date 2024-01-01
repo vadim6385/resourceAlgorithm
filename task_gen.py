073325568e3a61124aa316ddb4ae6df0a5a18674
Vadim Darchuk <vadim.darchuk@wdc.com>
2024-01-01 12:59:24 +0200

    fix bug with random task generation

--- task_gen.py
+++ task_gen.py
@@ -6,11 +6,11 @@ from task import TaskPriority, Task
 from utils import DEFAULT_END_TIME, DEBUG_HALT
 
 
-def generate_random_tasks(num_tasks, max_bandwidth, start_time=0, end_time=DEFAULT_END_TIME, priority=None, sort_tasks=True):
+def generate_random_tasks(num_tasks, max_bandwidth, start_time=0, end_time=DEFAULT_END_TIME, set_priority=None, sort_tasks=True):
     """
     generate queue of random tasks
     :param sort_tasks: sort tasks by created time, default True
-    :param priority: set priority, by default priority is set at random
+    :param set_priority: set priority, by default priority is set at random
     :param num_tasks: number of tasks to generate
     :param max_bandwidth: maximum bandwidth for the task
     :param start_time: global task start time
@@ -21,16 +21,17 @@ def generate_random_tasks(num_tasks, max_bandwidth, start_time=0, end_time=DEFAU
     for i in range(num_tasks):
         task_bandwidth = random.randint(0, int(max_bandwidth / 2))
         task_min_bandwidth = random.randint(0, task_bandwidth)
-        if not priority:
+        if not set_priority:
             priority = random.randrange(TaskPriority.REGULAR, TaskPriority.ENTERPRISE + 1, 1)
+        else:
+            priority = set_priority
         task_created_time = random.randint(start_time, int(end_time - 1))
         max_duration = end_time - task_created_time
         duration = random.randint(1, max_duration)  # Use max_duration as the upper limit
         if (task_created_time + duration) > end_time:
             DEBUG_HALT()
         new_task = Task(bandwidth=task_bandwidth, created_time=task_created_time,
-                        duration=duration, min_bandwidth=task_min_bandwidth)
-        new_task.priority = priority
+                        duration=duration, min_bandwidth=task_min_bandwidth, priority=priority)
         ret.append(new_task)
     return sorted(ret, key=attrgetter('created_time')) if sort_tasks else ret
 
@@ -99,13 +100,13 @@ def gen_tasks_lowest_priority_first(num_tasks, max_bandwidth, start_time=0, end_
     # Each list is sorted by some criterion within generate_random_tasks function
     reg_tasks_list = generate_random_tasks(num_tasks=num_tasks_reg_prio, max_bandwidth=max_bandwidth,
                                            start_time=start_time_reg_prio, end_time=end_time_reg_prio,
-                                           priority=TaskPriority.REGULAR, sort_tasks=True)
+                                           set_priority=TaskPriority.REGULAR, sort_tasks=True)
     prem_tasks_list = generate_random_tasks(num_tasks=num_tasks_prem_prio, max_bandwidth=max_bandwidth,
                                             start_time=start_time_prem_prio, end_time=end_time_prem_prio,
-                                            priority=TaskPriority.PREMIUM, sort_tasks=True)
+                                            set_priority=TaskPriority.PREMIUM, sort_tasks=True)
     ent_tasks_list = generate_random_tasks(num_tasks=num_tasks_ent_prio, max_bandwidth=max_bandwidth,
                                            start_time=start_time_ent_prio, end_time=end_time_ent_prio,
-                                           priority=TaskPriority.ENTERPRISE, sort_tasks=True)
+                                           set_priority=TaskPriority.ENTERPRISE, sort_tasks=True)
 
     # Return a combined list of tasks, starting with the lowest priority (regular) to the highest (enterprise)
     return sorted(reg_tasks_list + prem_tasks_list + ent_tasks_list, key=attrgetter('created_time'))
@@ -136,13 +137,13 @@ def gen_tasks_reg_and_prem_first_ent_last(num_tasks, max_bandwidth, start_time=0
 
     reg_tasks_list = generate_random_tasks(num_tasks=num_tasks_reg_prio, max_bandwidth=max_bandwidth,
                                            start_time=start_time_reg_prem_prio, end_time=end_time_reg_prem_prio,
-                                           priority=TaskPriority.REGULAR, sort_tasks=True)
+                                           set_priority=TaskPriority.REGULAR, sort_tasks=True)
     prem_tasks_list = generate_random_tasks(num_tasks=num_tasks_prem_prio, max_bandwidth=max_bandwidth,
                                             start_time=start_time_reg_prem_prio, end_time=end_time_reg_prem_prio,
-                                            priority=TaskPriority.PREMIUM, sort_tasks=True)
+                                            set_priority=TaskPriority.PREMIUM, sort_tasks=True)
     ent_tasks_list = generate_random_tasks(num_tasks=num_tasks_ent_prio, max_bandwidth=max_bandwidth,
                                            start_time=start_time_ent_prio, end_time=end_time_ent_prio,
-                                           priority=TaskPriority.ENTERPRISE, sort_tasks=True)
+                                           set_priority=TaskPriority.ENTERPRISE, sort_tasks=True)
 
     # Return a combined list of tasks, starting with the lowest priority (regular) to the highest (enterprise)
     return sorted(reg_tasks_list + prem_tasks_list + ent_tasks_list, key=attrgetter('created_time'))
